170bca475261715c53e4e0885008f42e641ad51b
sjkim1998 <jskim1998@outlook.com>
2017-10-14 09:42:41 -0400

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,40 +1,24 @@
-// Page Scroll
-jQuery(document).ready(function ($) {
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-			|| location.hostname == this.hostname) {
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top - 32
-				}, 1000);
-				return false;
-			}
-		}
-	});
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
-
-// Fixed Nav
-jQuery(document).ready(function ($) {
-	$(window).scroll(function(){
-		var scrollTop = 142;
-		if($(window).scrollTop() >= scrollTop){
-			$('nav').css({
-				position : 'fixed',
-				top : '0'
-			});
-		}
-		if($(window).scrollTop() < scrollTop){
-			$('nav').removeAttr('style');	
-		}
-	})
-  
-  // Active Nav Link
-  $('nav ul li a').click(function(){
-         $('nav ul li a').removeClass('active');
-         $(this).addClass('active');
-    });
-})
-
